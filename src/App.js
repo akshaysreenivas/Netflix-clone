3b14492f651d5ff5ccee664855d8b34418758f63
akshay <akshaypvkeerakkal@gmail.com>
2022-03-10 16:01:30 +0530

    dummy datas added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/banner/Banner';
 import Navbar from './components/NavBar/Navbar';
+import Rowpost from './rowpost/Rowpost';
 
 function App() {
   return (
     <div className="">
      <Navbar/>
      <Banner/>
+     <Rowpost/>
     </div>
   );
 }
