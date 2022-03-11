21cad9780851dda3b170cb8d073b87b412e24538
akshay <akshaypvkeerakkal@gmail.com>
2022-03-11 14:45:20 +0530

    generes added with api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import Banner from './components/banner/Banner';
 import Navbar from './components/NavBar/Navbar';
 import Rowpost from './components/rowpost/Rowpost';
-import { Action, Originals } from './url/Urls';
-
+import { Action, Originals,Trending,Romance,Comedy,Horror,Documentaries } from './url/Urls';
+import React from 'react';
 function App() {
   return (
     <div className="">
      <Navbar/>
      <Banner/>
      <Rowpost url={Originals} title="Netflix Originals"  />
+     <Rowpost url={Trending} title="Trending Now" isSmall />
      <Rowpost url={Action} title="Action" isSmall />
+     <Rowpost url={Romance} title="Romance" isSmall />
+     <Rowpost url={Comedy} title="Comedy" isSmall />
+     <Rowpost url={Horror} title="Horror" isSmall />
+     <Rowpost url={Documentaries} title="Documentaries" isSmall />
     </div>
+    
   );
 }
 
